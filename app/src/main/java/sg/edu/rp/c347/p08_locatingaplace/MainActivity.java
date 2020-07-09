b6c06e543b0dad6857003b3db5a1f6b9c67f9f80
KarenKuan <18055889@myrp.edu.sg>
2020-07-09 12:32:17 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c347/p08_locatingaplace/MainActivity.java
@@ -0,0 +1,23 @@
+package sg.edu.rp.c347.p08_locatingaplace;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+import com.google.android.gms.maps.GoogleMap;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnnormal, btnterrain, btnsatelite;
+    private GoogleMap map;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FragmentManager
+    }
+}
